0dcb480df6648ccb28cf23d6dbbc4cac0c40f847
Ashikur Rahman Shad <shadgood@gmail.com>
2024-04-12 16:44:10 +0600

    Kawai UI fix + page updates

--- frontend/script/food-info.js
+++ frontend/script/food-info.js
@@ -1,15 +1,14 @@
-var latestFoodDiv = document.getElementById("latest-food");
-
-var food_id = GET()['food_id'];
-
 async function food_info(){
+    var food_id = GET()['food_id'];
     var food = await fetchJSON("http://localhost/food-ordering-system-backend/api/common/view-food.php?food_id="+food_id);
+    var restaurant = await fetchJSON("http://localhost/food-ordering-system-backend/api/common/restaurant-profile.php?restaurant_id="+food['restaurant_id'])
 
 
     output("food_name",food['name']);
     output("food_price",food['price']);
+    output("restaurant_name", restaurant['name']);
     output("description",food['description']);
-    document.getElementById("food_img").src= food['photo_link'];
+    $("#food_img").src= food['photo_link'];
 }
 
 food_info();
