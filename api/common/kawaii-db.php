e0f9771969a1f45b93dff67f0280c2912b8d8ade
Ashikur Rahman Shad <ashikur.rahman.shad.arstrx@gmail.com>
2024-12-06 18:53:22 +0600

    show php errors

--- api/common/kawaii-db.php
+++ api/common/kawaii-db.php
@@ -8,6 +8,11 @@ const database = "food-ordering-system";
 
 date_default_timezone_set('Asia/Dhaka');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 
 
 //don't edit below this section. The is the main kawaii-php
@@ -16,66 +21,6 @@ if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
 
-class kawaii_db
-{
-
-  private $db_host;
-  private $db_user;
-  private $db_pass;
-  private $db_name;
-
-  function __construct($host, $username, $password, $database)
-  {
-    $this->db_host = $host;
-    $this->db_user = $username;
-    $this->db_pass = $password;
-    $this->db_name = $database;
-  }
-
-  function sql($query)
-  {
-    // global $db_host, $db_user, $db_pass, $db_name;
-    $conn = mysqli_connect($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
-    $result = mysqli_query($conn, $query);
-    mysqli_close($conn);
-    return $result;
-  }
-
-  function sql_fetch($query)
-  {
-    return mysqli_fetch_all($this->sql($query), MYSQLI_ASSOC);
-  }
-
-  function sql_row($query)
-  {
-    $x = $this->sql_fetch($query);
-    if (count($x) == 1)
-      return $x[0];
-  }
-
-  function match_table($table, $key_value_array = null, $parm = "*")
-  {
-    $query = "SELECT * FROM {$table}";
-
-    if ($key_value_array != null) {
-
-      $conditions = [];
-
-      foreach ($key_value_array as $key => $value)
-        $conditions[] = "`$key` = '$value'";
-
-      $query .= " WHERE " . implode(" AND ", $conditions) . ";";
-    }
-    return $this->sql_fetch($query);
-  }
-
-  function match_row($table, $key_value_array = null, $parm = "*")
-  {
-    $x = $this->match_table($table, $key_value_array, $parm);
-    if (count($x) == 1)
-      return $x[0];
-  }
-}
 
 function post($elements)
 {
@@ -119,6 +64,7 @@ function get($elements)
 function sql($query)
 {
   // global db_host, db_user, db_pass, db_name;
+  
   $conn = mysqli_connect(db_host, db_user, db_pass, database);
   $result = mysqli_query($conn, $query);
   mysqli_close($conn);
