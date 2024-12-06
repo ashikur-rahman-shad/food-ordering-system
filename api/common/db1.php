e755b6e4cbd7cc3a39fad0513488592d59af0473
Ashikur Rahman Shad <ashikur.rahman.shad.arstrx@gmail.com>
2024-12-06 18:52:40 +0600

    Useless object removed

--- api/common/db1.php
+++ api/common/db1.php
@@ -2,7 +2,6 @@
 
 require_once 'kawaii-db.php';
 
-$db = new kawaii_db(db_host, db_user, db_pass, database);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
